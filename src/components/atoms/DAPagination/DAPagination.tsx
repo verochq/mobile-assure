35350ccd5f220962d4ca3613dbdfec06e5d92c7d
Veronica <veronica.chacon@assuresoft.tech>
2025-10-13 07:51:48 -0400

    Actualizar DAPagination.tsx

--- src/components/atoms/DAPagination/DAPagination.tsx
+++ src/components/atoms/DAPagination/DAPagination.tsx
@@ -1,25 +1,24 @@
 import React from 'react';
-import { View, ViewProps } from 'react-native';
-
+import { View } from 'react-native';
+import { styles } from './style'; 
 interface DAPaginationProps {
-  count: number; 
+  count: number;
   index: number;
-  style?: ViewProps['style'];
 }
 
-export const DAPagination = ({ count, index, style }: DAPaginationProps) => {
+export const DAPagination = ({ count, index }: DAPaginationProps) => {
   return (
-    <View style={style}>
+    <View style={styles.container}>
       {Array.from({ length: count }).map((_, i) => (
         <View
           key={i}
-          style={{
-            width: 6,
-            height: 6,
-            borderRadius: 3,
-            marginHorizontal: 4,
-            backgroundColor: i === index ? '#FFC107' : 'rgba(255, 255, 255, 0.4)',
-          }}
+          style={[
+            styles.dot,
+            {
+              backgroundColor:
+                i === index ? '#FFC107' : 'rgba(255, 255, 255, 0.4)',
+            },
+          ]}
         />
       ))}
     </View>

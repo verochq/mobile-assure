73bf5e979ca19771b67b1a959f0d1525ac2ea586
Veronica <veronica.chacon@assuresoft.tech>
2025-10-13 07:50:49 -0400

    Crear style.ts de DAPagination

---
+++ src/components/atoms/DAPagination/style.ts
@@ -0,0 +1,15 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  dot: {
+    width: 6,
+    height: 6,
+    borderRadius: 3,
+    marginHorizontal: 4,
+  },
+});
